249529afdd453b1f2e40659e4bb0fb98493e3bad
ebront08 <ebront08@gmail.com>
2021-10-13 10:34:42 -0400

    added code that will find product by single and id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -21,6 +21,19 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productDataById = await Product.findByPk(req.params.id, {
+        include: [{ model: Category, as: 'category' }, { model: Tag, through: ProductTag, as: 'tagIds' }]
+    });
+
+    if (!productDataById) {
+        res.status(404).json({ message: 'No product found with that id!' });
+        return;
+    }
+    res.status(200).json(productDataById);
+} catch (err) {
+    res.status(500).json(err);
+}
 });
 
 // create new product
