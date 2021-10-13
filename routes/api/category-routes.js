1ecf8bc2131f3e7c33839fc604b4e4d6a80975ab
ebront08 <ebront08@gmail.com>
2021-10-13 10:27:19 -0400

    this will update a category by its id value with the router.put

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,6 +48,22 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    const updatedCategory = await Category.update({
+        category_name: req.body.category_name,
+    }, {
+        where: {
+            id: req.params.id
+        },
+    });
+    if (!updatedCategory) {
+        res.status(404).json({ message: "No category found with that id!" });
+        return;
+    }
+    res.status(200).json(updatedCategory);
+} catch (err) {
+    res.status(500).json(err);
+}
 });
 
 router.delete('/:id', (req, res) => {
