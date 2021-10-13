ac73476dff968040913be8c13127cce09242ae0b
ebront08 <ebront08@gmail.com>
2021-10-13 09:18:26 -0400

    accidentally added the develop folder from mini project shouldve added from homework

--- models/index.js
+++ models/index.js
@@ -1,23 +1,20 @@
-const Reader = require('./Reader');
-const Book = require('./Book');
-const LibraryCard = require('./LibraryCard');
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
 
-Reader.hasOne(LibraryCard, {
-  foreignKey: 'reader_id',
-  onDelete: 'CASCADE',
-});
+// Products belongsTo Category
 
-Reader.hasMany(Book, {
-  foreignKey: 'reader_id',
-  onDelete: 'CASCADE',
-});
+// Categories have many Products
 
-Book.belongsTo(Reader, {
-  foreignKey: 'reader_id',
-});
+// Products belongToMany Tags (through ProductTag)
 
-LibraryCard.belongsTo(Reader, {
-  foreignKey: 'reader_id',
-});
+// Tags belongToMany Products (through ProductTag)
 
-module.exports = { Reader, Book, LibraryCard };
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
