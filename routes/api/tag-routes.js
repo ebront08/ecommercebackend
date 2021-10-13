40b0b0e0fda14b08b669f98514304402cbc3d84c
ebront08 <ebront08@gmail.com>
2021-10-13 11:14:39 -0400

    added new tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -19,10 +19,30 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagDataById = await Tag.findByPk(req.params.id, {
+        include: [{ model: Product, through: ProductTag, as: 'taggedProducts' }],
+    });
+
+    if (!tagDataById) {
+        res.status(404).json({ message: 'No tag found with that id!' });
+        return;
+    }
+    res.status(200).json(tagDataById);
+} catch (err) {
+    res.status(500).json(err);
+}
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  try {
+    const newTagData = await Tag.create();
+
+    res.status(201).json(newTagData);
+} catch (err) {
+    req.status(500).json(err);
+}
 });
 
 router.put('/:id', (req, res) => {
